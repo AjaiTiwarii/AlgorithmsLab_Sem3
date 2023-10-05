effd2d3c4d0c848f86be01f8a3710e12ec6f8980
AjaiTiwarii <ajait0209@gmail.com>
2023-10-05 22:47:10 +0530

    Karatsuba algorithm

---
+++ CSC312Assignment - 8[862].cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long power(long s) {
+    long long result = 1;
+    for (int i = 0; i < s; i++) {
+        result *= 10;
+    }
+    return result;
+}
+
+long numlen(long t) {
+    long length = 0;
+    while (t > 0) {
+        length++;
+        t /= 10;
+    }
+    return length;
+}
+
+long long karatsuba(long long x, long long y) {
+    if (x < 10 || y < 10) {
+        return x * y;
+    }
+
+    long n = max(numlen(x), numlen(y));
+    long n2 = n / 2;
+
+    long long x1 = x / power(n2);
+    long long x2 = x % power(n2);
+    long long y1 = y / power(n2);
+    long long y2 = y % power(n2);
+
+    long long x1y1 = karatsuba(x1, y1);
+    long long x2y2 = karatsuba(x2, y2);
+    // long long ad_bc = karatsuba(a + b, c + d) - ac - bd;/
+    long long x1y2_x2y1 = x1y1 + x2y2 - karatsuba(x1 - x2, y1 - y2);
+
+    return x1y1 * power(2 * n2) + x1y2_x2y1 * power(n2) + x2y2;
+}
+
+int main() {
+    // long long a = 1234567890123;
+    long long a = 1000;
+    // long long b = 45678912345670;
+    long long b = 2000;
+
+    long long answer = karatsuba(a, b);
+
+    cout << "Multiplication result: " << answer  << endl;
+
+    return 0;
+}
+
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class cmp {
+public:
+    bool operator()(Node* a, Node* b) {
+        return a->data > b->data;
+    }
+};
+
+class Solution {
+public:
+    void traverse(Node* root, vector<string>& ans, string temp) {
+        // Base case
+        if (root->left == NULL && root->right == NULL) {
+            ans.push_back(temp);
+            return;
+        }
+
+        traverse(root->left, ans, temp + '0');
+        traverse(root->right, ans, temp + '1');
+    }
+
+    vector<string> huffmanCodes(string S, vector<int> f, int N) {
+        // Declaration of priority_queue
+        // priority_queue<DataType, ContainerType, CompareFunction> pq;
+        priority_queue<Node*, vector<Node*>, cmp> pq;
+
+        for (int i = 0; i < N; i++) {
+            Node* temp = new Node(f[i]);
+            pq.push(temp);
+        }
+
+        while (pq.size() > 1) {
+            Node* left = pq.top();
+            pq.pop();
+
+            Node* right = pq.top();
+            pq.pop();
+
+            Node* newNode = new Node(left->data + right->data);
+            newNode->left = left;
+            newNode->right = right;
+            pq.push(newNode);
+        }
+
+        Node* root = pq.top();
+        vector<string> ans;
+        string temp = "";
+        traverse(root, ans, temp);
+        return ans;
+    }
+};
+
+int main() {
+    string input = "The rupee opened stronger after the dollar index fell to below hundred and ten levels, said Anil Kumar Bhansali, Head of Treasury, Finrex Treasury Advisors.";
+    map<char, int> frequencies;
+
+    // Calculate character frequencies
+    for (char c : input) {
+        if (c != ' ') {
+            frequencies[c]++;
+        }
+    }
+
+    // characters and frequencies ke liye array bna rhe
+    vector<char> characters;
+    vector<int> freq_values;
+
+    for (auto entry : frequencies) {
+        characters.push_back(entry.first);
+        freq_values.push_back(entry.second);
+    }
+
+    int N = characters.size();
+
+    for (int i = 0; i < N; i++) {
+        cout << "'" << characters[i] << "': " << freq_values[i] << endl;
+    }
+
+    Solution solution;
+
+    vector<string> huffmanCodes = solution.huffmanCodes(input, freq_values, N);
+
+    cout << "---------------------------" << endl<< endl;
+    cout << "Character Huffman Codes:" << endl<<endl;
+    cout << "---------------------------" << endl;
+
+    for (int i = 0; i < N; i++) {
+        cout << "'" << characters[i] << "': " << huffmanCodes[i] << endl;
+    }
+
+    return 0;
+}
